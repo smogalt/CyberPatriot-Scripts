2ef97043b6facf4e470b246ae5761d8467bd6ddc
smogalt <107054973+smogalt@users.noreply.github.com>
2022-11-30 19:50:35 -0500

    Initial Commit

---
+++ package_sniffer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Putting the names of all installed packages into a temporary files.
+apt list --installed > pkgnames_temp.tx
+
+# Set buffer for command arguement
+input_buffer="$1"
+
+# Require a command arguement to make sure that there isn't a bunch of output
+if [[ $1 == "" ]]; then
+	input_buffer="games"
+fi
+
+
+# While loop to read line by line.
+while IFS= read -r line; do
+	
+	# Reading lines
+	line=${line%%/*}
+	
+	# Running apt-cache show for each line
+	buffer=`apt-cache show "$line"`
+	
+	# Checking if the provided arguement is in the buffer
+	if [[ $buffer == *"$input_buffer"* ]]; then
+		echo "$line";
+	fi
+done < pkgnames_temp.txt
+rm pkgnames_temp.txt
